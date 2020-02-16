3f027797044b12728a177a7ca8a9ff7eedecab99
DimaR14 <60104591+DimaR14@users.noreply.github.com>
2020-02-16 19:25:39 +0200

    1

--- src/task1/task1.java
+++ src/task1/task1.java
@@ -6,19 +6,19 @@ public class task1 {
 		Point a = new Point(6.5,4.6);
 		Point b = new Point(3.6,7.1);
 		Point c = new Point(1.2,2.1);
-		Triangle abc = new Triangle(a,b,c);
+		Shape abc = new Triangle(a,b,c);
 		System.out.println(abc.toString());
 		
 		Point a1 = new Point(2.5,3.5);
 		Point b1 = new Point(9.2,3.5);
 		Point c1 = new Point(2.5,8.3);
 		Point d1 = new Point(9.2,8.3);
-		Rectangle abcd = new Rectangle(a1,b1,c1,d1); 
+		Shape abcd = new Rectangle(a1,b1,c1,d1); 
 		System.out.println(abcd.toString());
 		
 		Point a2 = new Point(3.5,2.1);
 		Point b2 = new Point(6.4,4.7);
-		Circle ab = new Circle(a2,b2);
+		Shape ab = new Circle(a2,b2);
 		System.out.println(ab.toString());
 	}
 }
